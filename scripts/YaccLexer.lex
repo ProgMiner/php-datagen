ca261917729211c7fff70c64ad91534e3f9e11e0
ProgMiner <eridan200@mail.ru>
2018-08-01 04:59:10 +0300

    Add lexer for yacc lang

---
+++ scripts/YaccLexer.lex
@@ -0,0 +1,61 @@
+<?php
+
+// vim:noexpandtab
+
+%%
+
+%{
+	private function handleReduceCB() {
+		static $bracesDepth = 0;
+
+		if ($bracesDepth === 0) {
+			$this->yybegin(self::REDUCE_CB);
+		}
+
+		switch ($this->yytext()) {
+		case '{':
+			++$bracesDepth;
+			break;
+
+		case '}':
+			--$bracesDepth;
+			break;
+		}
+
+		if ($bracesDepth === 0) {
+			$this->yybegin(self::REDUCE_CB);
+		}
+
+		return $this->createToken('T_REDUCE_CB');
+	}
+%}
+
+%class YaccLexer
+%function nextToken
+
+%line
+%char
+
+%state REDUCE_CB
+
+W	= [a-zA-Z_]
+N	= [0-9]
+S	= [ \b\n\t\f\r]
+
+L = {W}({W}|{N})*
+
+%%
+
+<REDUCE_CB> \'(\\.|[^\\\'])*\'|\"(\\.|[^\\\"])*\"|.|{L}|{S}	{
+	return $this->handleReduceCB();
+}
+
+{S}+	{ return $this->createToken('GAP'); }
+"//".*	{ return $this->createToken('T_COMMENT'); }
+
+<YYINITIAL> "%".*	{ return $this->createToken('T_STMT'); }
+
+<YYINITIAL> "{"	{ return $this->handleReduceCB(); }
+
+<YYINITIAL>	{L}	{ return $this->createToken('T_LITERAL'); }
+<YYINITIAL>	.	{ return $this->createToken(); }
---
+++ scripts/YaccLexer.php
@@ -0,0 +1,265 @@
+<?php
+// vim:noexpandtab
+
+class YaccLexer extends \JLexPHP\Base  {
+	const YY_BUFFER_SIZE = 512;
+	const YY_F = -1;
+	const YY_NO_STATE = -1;
+	const YY_NOT_ACCEPT = 0;
+	const YY_START = 1;
+	const YY_END = 2;
+	const YY_NO_ANCHOR = 4;
+	const YY_BOL = 128;
+	const YY_EOF = 129;
+
+	private function handleReduceCB() {
+		static $bracesDepth = 0;
+		if ($bracesDepth === 0) {
+			$this->yybegin(self::REDUCE_CB);
+		}
+		switch ($this->yytext()) {
+		case '{':
+			++$bracesDepth;
+			break;
+		case '}':
+			--$bracesDepth;
+			break;
+		}
+		if ($bracesDepth === 0) {
+			$this->yybegin(self::REDUCE_CB);
+		}
+		return $this->createToken('T_REDUCE_CB');
+	}
+	protected $yy_count_chars = true;
+	protected $yy_count_lines = true;
+
+	public function __construct($stream) {
+		parent::__construct($stream);
+		$this->yy_lexical_state = self::YYINITIAL;
+	}
+
+	const YYINITIAL = 0;
+	const REDUCE_CB = 1;
+	static $yy_state_dtrans = [
+		0,
+		9
+	];
+	static $yy_acpt = [
+		/* 0 */ self::YY_NOT_ACCEPT,
+		/* 1 */ self::YY_NO_ANCHOR,
+		/* 2 */ self::YY_NO_ANCHOR,
+		/* 3 */ self::YY_NO_ANCHOR,
+		/* 4 */ self::YY_NO_ANCHOR,
+		/* 5 */ self::YY_NO_ANCHOR,
+		/* 6 */ self::YY_NO_ANCHOR,
+		/* 7 */ self::YY_NO_ANCHOR,
+		/* 8 */ self::YY_NO_ANCHOR,
+		/* 9 */ self::YY_NOT_ACCEPT,
+		/* 10 */ self::YY_NO_ANCHOR,
+		/* 11 */ self::YY_NO_ANCHOR,
+		/* 12 */ self::YY_NOT_ACCEPT,
+		/* 13 */ self::YY_NO_ANCHOR,
+		/* 14 */ self::YY_NOT_ACCEPT,
+		/* 15 */ self::YY_NO_ANCHOR,
+		/* 16 */ self::YY_NOT_ACCEPT,
+		/* 17 */ self::YY_NO_ANCHOR,
+		/* 18 */ self::YY_NO_ANCHOR,
+		/* 19 */ self::YY_NOT_ACCEPT
+	];
+		static $yy_cmap = [
+ 3, 3, 3, 3, 3, 3, 3, 3, 8, 8, 4, 3, 8, 4, 3, 3, 3, 3, 3, 3,
+ 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 8, 3, 5, 3, 3, 10, 3, 1,
+ 3, 3, 3, 3, 3, 3, 3, 9, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 3, 3,
+ 3, 3, 3, 3, 3, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6,
+ 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 3, 2, 3, 3, 6, 3, 6, 6, 6,
+ 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6,
+ 6, 6, 6, 11, 3, 3, 3, 3, 0, 0,];
+
+		static $yy_rmap = [
+ 0, 1, 1, 2, 3, 4, 1, 5, 6, 7, 8, 1, 9, 2, 6, 10, 10, 11, 8, 12,
+];
+
+		static $yy_nxt = [
+[
+ 1, 2, 2, 2, 3, 2, 4, 2, 3, 10, 5, 6,
+],
+[
+ -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
+],
+[
+ -1, -1, -1, -1, 3, -1, -1, -1, 3, -1, -1, -1,
+],
+[
+ -1, -1, -1, -1, -1, -1, 4, 4, -1, -1, -1, -1,
+],
+[
+ -1, 5, 5, 5, -1, 5, 5, 5, 5, 5, 5, 5,
+],
+[
+ -1, 7, 7, 7, -1, 7, 7, 7, 7, 7, 7, 7,
+],
+[
+ -1, 11, 12, 14, 14, 14, 14, 14, 14, 14, 14, 14,
+],
+[
+ 1, 8, 11, 11, 13, 15, 17, 11, 13, 18, 11, 11,
+],
+[
+ -1, -1, -1, -1, -1, -1, -1, -1, -1, 7, -1, -1,
+],
+[
+ -1, 14, 14, 14, -1, 14, 14, 14, 14, 14, 14, 14,
+],
+[
+ -1, 16, 19, 16, 16, 11, 16, 16, 16, 16, 16, 16,
+],
+[
+ -1, -1, -1, -1, -1, -1, 17, 17, -1, -1, -1, -1,
+],
+[
+ -1, 16, 16, 16, -1, 16, 16, 16, 16, 16, 16, 16,
+],
+];
+
+	public function /*Yytoken*/ nextToken () {
+		$yy_anchor = self::YY_NO_ANCHOR;
+		$yy_state = self::$yy_state_dtrans[$this->yy_lexical_state];
+		$yy_next_state = self::YY_NO_STATE;
+		$yy_last_accept_state = self::YY_NO_STATE;
+		$yy_initial = true;
+
+		$this->yy_mark_start();
+
+		$yy_this_accept = self::$yy_acpt[$yy_state];
+		if (self::YY_NOT_ACCEPT !== $yy_this_accept) {
+			$yy_last_accept_state = $yy_state;
+			$this->yy_mark_end();
+		}
+
+		while (true) {
+			$yy_lookahead = self::YY_BOL;
+
+			if (!$yy_initial || !$this->yy_at_bol) {
+				$yy_lookahead = $this->yy_advance();
+			}
+
+			$yy_next_state = self::$yy_nxt[self::$yy_rmap[$yy_state]][self::$yy_cmap[$yy_lookahead]];
+
+			if (self::YY_EOF === $yy_lookahead && $yy_initial) {
+				return null;
+			}
+
+			if (self::YY_F !== $yy_next_state) {
+				$yy_state = $yy_next_state;
+				$yy_initial = false;
+
+				$yy_this_accept = self::$yy_acpt[$yy_state];
+				if (self::YY_NOT_ACCEPT !== $yy_this_accept) {
+					$yy_last_accept_state = $yy_state;
+					$this->yy_mark_end();
+				}
+			} else {
+				if (self::YY_NO_STATE === $yy_last_accept_state) {
+					throw new \Exception("Lexical Error: Unmatched Input.");
+				} else {
+					$yy_anchor = self::$yy_acpt[$yy_last_accept_state];
+
+					if (0 !== (self::YY_END & $yy_anchor)) {
+						$this->yy_move_end();
+					}
+
+					$this->yy_to_mark();
+
+					switch ($yy_last_accept_state) {
+					case 1: 
+					case -2:
+						break;
+
+					case 2: { return $this->createToken(); }
+					case -3:
+						break;
+
+					case 3: { return $this->createToken('GAP'); }
+					case -4:
+						break;
+
+					case 4: { return $this->createToken('T_LITERAL'); }
+					case -5:
+						break;
+
+					case 5: { return $this->createToken('T_STMT'); }
+					case -6:
+						break;
+
+					case 6: { return $this->handleReduceCB(); }
+					case -7:
+						break;
+
+					case 7: { return $this->createToken('T_COMMENT'); }
+					case -8:
+						break;
+
+					case 8: {
+	return $this->handleReduceCB();
+}
+					case -9:
+						break;
+
+					case 10: { return $this->createToken(); }
+					case -10:
+						break;
+
+					case 11: {
+	return $this->handleReduceCB();
+}
+					case -11:
+						break;
+
+					case 13: {
+	return $this->handleReduceCB();
+}
+					case -12:
+						break;
+
+					case 15: {
+	return $this->handleReduceCB();
+}
+					case -13:
+						break;
+
+					case 17: {
+	return $this->handleReduceCB();
+}
+					case -14:
+						break;
+
+					case 18: {
+	return $this->handleReduceCB();
+}
+					case -15:
+						break;
+
+						default:
+							$this->yy_error('INTERNAL', false);
+
+						case -1:
+
+					}
+
+					$yy_initial = true;
+					$yy_state = self::$yy_state_dtrans[$this->yy_lexical_state];
+					$yy_next_state = self::YY_NO_STATE;
+					$yy_last_accept_state = self::YY_NO_STATE;
+
+					$this->yy_mark_start();
+
+					$yy_this_accept = self::$yy_acpt[$yy_state];
+					if (self::YY_NOT_ACCEPT !== $yy_this_accept) {
+						$yy_last_accept_state = $yy_state;
+						$this->yy_mark_end();
+					}
+				}
+			}
+		}
+	}
+}
